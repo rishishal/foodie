2357f3dbf97bf4a8325dcd5472065616dff5f37a
rishishal <rishi.kumar1609@gmail.com>
2024-04-04 17:15:12 +0530

    cart UI update

--- src/page/Cart.jsx
+++ src/page/Cart.jsx
@@ -1,21 +1,19 @@
-import { useEffect, useState } from "react"
-import {useSelector, useDispatch} from "react-redux"
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import { MEDIA_ASSETS } from "../utils/constans";
-import toast from 'react-hot-toast';
+import toast from "react-hot-toast";
 import CartItem from "../components/CartItem";
 import { clearCart } from "../utils/CartSlice";
 import { IoTimerSharp } from "react-icons/io5";
-
 
 const Cart = () => {
   const dispatch = useDispatch();
   const [totalPrice, setTotalPrice] = useState(0);
   const [totalAmount, setTotalAmount] = useState(0);
 
-  const cartItems = useSelector((store)=>store.cart.items)
-  const restaurantInfo = useSelector((store)=> store.cart.restaurant)
-
+  const cartItems = useSelector((store) => store.cart.items);
+  const restaurantInfo = useSelector((store) => store.cart.restaurant);
 
   useEffect(() => {
     // Calculate total price when cartItems change
@@ -23,8 +21,8 @@ const Cart = () => {
       let total = 0;
       cartItems.forEach((item) => {
         total += item.card.info.price
-          ? item.card.info.price / 100 * item.quantity
-          : item.card.info.defaultPrice / 100 * item.quantity;
+          ? (item.card.info.price / 100) * item.quantity
+          : (item.card.info.defaultPrice / 100) * item.quantity;
       });
       total = Number(total.toFixed(2));
       setTotalPrice(total);
@@ -39,13 +37,17 @@ const Cart = () => {
       // Add GST and restaurant charges to totalPrice
       const gstAndCharges = Number((totalPrice * 0.18).toFixed(2)); // Assuming GST is 18%
 
-      console.log(restaurantInfo?.feeDetails?.totalFee / 100 || 0)
+      console.log(restaurantInfo?.feeDetails?.totalFee / 100 || 0);
 
       // Update the total amount
-      const finalTotalAmount = Number((totalPrice + (restaurantInfo?.feeDetails?.totalFee / 100 || 0) + gstAndCharges).toFixed(2));
-      setTotalAmount(
-        (finalTotalAmount)
+      const finalTotalAmount = Number(
+        (
+          totalPrice +
+          (restaurantInfo?.feeDetails?.totalFee / 100 || 0) +
+          gstAndCharges
+        ).toFixed(2)
       );
+      setTotalAmount(finalTotalAmount);
     };
 
     calculateTotalAmount();
@@ -55,62 +57,75 @@ const Cart = () => {
     dispatch(clearCart());
   };
 
-
-  return cartItems.length === 0?(
+  return cartItems.length === 0 ? (
     <div>
       <img
-      className="flex justify-center items-center m-auto w-1/4 h-1/4 my-12"
+        className="flex justify-center items-center m-auto md:w-1/4 md:h-1/4 my-12"
         src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/2xempty_cart_yfxml0"
         alt=""
       />
-      <h3 className="text-2xl font-bold text-gray-600 font-Mulish mb-1">Your cart is empty</h3>
-      <p className="font-Mulish text-base font-semibold mb-6">You can go to home page to view more restaurants</p>
-      <Link
-            className="btn-style btn-add"
-            to={"/"}
-          >
-            <button className="p-2 rounded-lg shadow-xl font-Mulish font-semibold text-white bg-[#F39A25]">SEE RESTAURANTS NEAR YOU</button>
-          </Link>
+      <h3 className="text-2xl font-bold text-gray-600 font-Mulish mb-1">
+        Your cart is empty
+      </h3>
+      <p className="font-Mulish text-base font-semibold mb-10">
+        You can go to home page to view more restaurants
+      </p>
+      <Link className="btn-style btn-add" to={"/"}>
+        <button className="p-2 rounded-lg shadow-xl font-Mulish font-semibold text-white bg-[#F39A25]">
+          SEE RESTAURANTS NEAR YOU
+        </button>
+      </Link>
     </div>
-  ):(
+  ) : (
     <div>
-      <h3 className="text-center m-6 text-3xl font-Raleway font-extrabold">Cart</h3>
+      <h3 className="text-center m-6 text-3xl font-Raleway font-extrabold">
+        Cart
+      </h3>
       <div className="my-5 mx-auto max-w-md rounded-xl border-2 p-4 border-[#F39A25]">
         <div className="flex justify-between items-center border-b-1 border-[#d3d3d3] mb-1 pb-4">
           <div className="font-Mulish text-left mb-4">
             <h3 className="text-lg font-bold">{restaurantInfo.name}</h3>
-            <p className="text-sm text-gray-600 font-semibold">{restaurantInfo.cuisines.join(", ")}</p>
+            <p className="text-sm text-gray-600 font-semibold">
+              {restaurantInfo.cuisines.join(", ")}
+            </p>
             <p className="text-sm text-gray-600 font-semibold">
               {restaurantInfo.locality},{" "}
               {restaurantInfo.sla.lastMileTravelString}
             </p>
             <div className="flex items-center gap-2 mt-2 font-Mulish">
-            <IoTimerSharp className="h-5 w-5"/>
+              <IoTimerSharp className="h-5 w-5" />
               <span className="text-xs font-semibold">
                 {restaurantInfo.sla.minDeliveryTime}-
                 {restaurantInfo.sla.maxDeliveryTime} MINS
               </span>
             </div>
           </div>
-          <img className="object-cover rounded-lg w-32 h-24" src={MEDIA_ASSETS + restaurantInfo.cloudinaryImageId} alt="" />
+          <img
+            className="object-cover rounded-lg w-32 h-24"
+            src={MEDIA_ASSETS + restaurantInfo.cloudinaryImageId}
+            alt=""
+          />
         </div>
         <div className="border-2 border-dashed border-[#d3d3d3] mb-4 rounded-xl">
           <CartItem items={cartItems} />
         </div>
 
-       
         <div className="flex justify-between">
-           <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-          onClick={() => {
-            handleClearCart();
-            toast.success("Cart cleared. Happy shopping")
-          }}
-          
-          > Clear Cart</button>
-          <Link
-            to={"/restaurants/" + restaurantInfo.id}
+          <button
+            type="button"
+            className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            onClick={() => {
+              handleClearCart();
+              toast.success("Cart cleared. Happy shopping");
+            }}
           >
-            <button className="focus:outline-none text-white bg-[#F39A25] hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2 me-2 mb-2 dark:focus:ring-yellow-900" >Add More Items</button>
+            {" "}
+            Clear Cart
+          </button>
+          <Link to={"/restaurants/" + restaurantInfo.id}>
+            <button className="focus:outline-none text-white bg-[#F39A25] hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2 me-2 mb-2 dark:focus:ring-yellow-900">
+              Add More Items
+            </button>
           </Link>
         </div>
         <div className="font-Mulish">
@@ -134,20 +149,26 @@ const Cart = () => {
         </div>
       </div>
       <div>
-        <Link onClick={() => {
-          toast("I am working on it!!", {
-            icon: '🙏',
-            style: {
-              textAlign: "center",
-            },
-            duration: 2500,
-          }),
-          handleClearCart();
-        }} to={"/"}>
-          <button className=" focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" >Place Order</button></Link>
+        <Link
+          onClick={() => {
+            toast("I am working on it!!", {
+              icon: "🙏",
+              style: {
+                textAlign: "center",
+              },
+              duration: 2500,
+            }),
+              handleClearCart();
+          }}
+          to={"/"}
+        >
+          <button className=" focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+            Place Order
+          </button>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
