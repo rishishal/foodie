a003dfd6df9226f9823c3fb07673e7513e1d2fe3
rishishal <rishi.kumar1609@gmail.com>
2024-04-04 14:50:37 +0530

    hearde Ui updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import Logo from "../assets/logo.jpg";
 import { useSelector } from "react-redux";
 import { FaLocationArrow } from "react-icons/fa";
+import { IoMenu } from "react-icons/io5";
 import { setLocation } from "../utils/LocationSlice";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import Nav from "./Nav";
+import { Nav, Navlinks } from "./Nav";
 import { Link } from "react-router-dom";
 
 const Header = () => {
@@ -12,6 +13,7 @@ const Header = () => {
   const City = useSelector((store) => store.data.city);
   const dispatch = useDispatch();
   const [nearMe, setNearMe] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleLocationNearMe = () => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -27,29 +29,46 @@ const Header = () => {
   );
 
   return (
-    <>
-      <div className="flex justify-between items-center font-Raleway">
-        <Link to="/">
-          <div className="w-20 h-10 flex">
-            <img src={Logo} alt="logo" />
-            <p className="font-extrabold text-4xl">Foodie</p>
-          </div>
-        </Link>
-
-        <div
-          className="w-2/4 flex gap-3 cursor-pointer"
-          onClick={handleLocationNearMe}
+    <nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-3">
+        <Link
+          to="/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
         >
-          <FaLocationArrow className="w-5 h-5 text-[#F39A25]" />
-          {nearMe ? (
-            <p className="font-extrabold font-Mulish">{City}</p>
-          ) : (
-            <p className="font-extrabold font-Mulish">Select Location</p>
-          )}
+          <img src={Logo} className="h-8" alt="Logo" />
+          <span className="self-center whitespace-nowrap font-Raleway font-extrabold text-4xl">
+            Foodie
+          </span>
+        </Link>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <button
+            type="button"
+            className="flex gap-1 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center text-black"
+            onClick={handleLocationNearMe}
+          >
+            <FaLocationArrow className="w-5 h-5 text-[#F39A25]" />
+            {nearMe ? (
+              <span className="font-extrabold font-Mulish">{City}</span>
+            ) : (
+              <span className="font-extrabold font-Mulish">
+                Select Location
+              </span>
+            )}
+          </button>
+          <button
+            data-collapse-toggle="navbar-sticky"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            aria-controls="navbar-sticky"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <IoMenu className="w-6 h-6" />
+          </button>
         </div>
-        <Nav totalQuantity={totalQuantity} /> 
+        <Nav totalQuantity={totalQuantity} />
+        {isOpen && <Navlinks totalQuantity={totalQuantity} />}
       </div>
-    </>
+    </nav>
   );
 };
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,12 @@
 import { FaShoppingCart } from "react-icons/fa";
 import { Link, NavLink } from "react-router-dom";
-const Nav = ({ totalQuantity }) => {
+export const Nav = ({ totalQuantity }) => {
   return (
-    <div>
-      <ul className="">
+    <div
+      className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+      id="navbar-sticky"
+    >
+      <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
         <li>
           <NavLink
             to="/"
@@ -28,14 +31,12 @@ const Nav = ({ totalQuantity }) => {
             About
           </NavLink>
         </li>
-        <li>
+        <li className="relative py-2 md:py-0">
           <Link to="/cart">
-            <div className="relative">
-              <FaShoppingCart className="h-5 w-5" />
-            </div>
-            <span className="absolute top-7 w-4 h-4 text-white text-xs rounded-full bg-[#F39A25] font-Mulish">
+            <span className="absolute -top-2.5 w-4 h-4 text-white text-xs rounded-full bg-[#F39A25] font-Mulish">
               {totalQuantity}
             </span>
+            <FaShoppingCart className="h-5 w-5 m-auto" />
           </Link>
         </li>
         <li>
@@ -51,4 +52,54 @@ const Nav = ({ totalQuantity }) => {
   );
 };
 
-export default Nav;
+export const Navlinks = ({ totalQuantity }) => {
+  return (
+    <div
+      className="items-center justify-between w-full md:flex md:w-auto md:order-1"
+      id="navbar-sticky"
+    >
+      <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `block py-2 pr-4 pl-3 duration-200 ${
+                isActive ? "text-[#F39A25]" : "text-gray-700"
+              } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-[#F39A25] lg:p-0`
+            }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              `block py-2 pr-4 pl-3 duration-200 ${
+                isActive ? "text-[#F39A25]" : "text-gray-700"
+              } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-[#F39A25] lg:p-0`
+            }
+          >
+            About
+          </NavLink>
+        </li>
+        <li className="relative py-2 md:py-0">
+          <Link to="/cart">
+            <span className="absolute top-0 w-4 h-4 text-white text-xs rounded-full bg-[#F39A25] font-Mulish">
+              {totalQuantity}
+            </span>
+            <FaShoppingCart className="h-5 w-5 m-auto" />
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="#"
+            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-[#F39A25] font-semibold rounded-lg  px-4 lg:px-1 py-2 lg:py-2.5 mr-2 focus:outline-none"
+          >
+            Log in
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
